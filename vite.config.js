64523472362bf1ace4bb0fd3ea28b675384d0be1
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-03-06 01:38:24 +0100

    build rollup added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PAL_Website_2023/'
+  base: '/PAL_Website_2023/',
+  build: {
+    rollupOptions: {
+      external: ['p5']
+    }
+  }
 })
